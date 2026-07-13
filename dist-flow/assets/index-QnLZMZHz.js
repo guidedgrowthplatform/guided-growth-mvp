e0e7f5438ac205ca868cac5d0b45af16ddf8aa8b
yair180 <yair@thefearless.life>
2026-07-13 15:34:13 +0300

    build(render): refresh complete contract artifacts

---
+++ dist-flow/assets/index-QnLZMZHz.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B6jTfnWx.js","assets/index-BIZM8yfu.js","assets/index-CPXzLC1X.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-BIZM8yfu.js";const n=r("Preferences",{web:()=>t(()=>import("./web-B6jTfnWx.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
