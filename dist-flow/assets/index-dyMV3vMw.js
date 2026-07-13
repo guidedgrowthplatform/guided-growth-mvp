b3ff2b20d81be5d32973e751afbe6765690e8124
yair180 <yair@thefearless.life>
2026-07-13 14:13:12 +0300

    build(render): regenerate provenance-stamped artifacts

--- dist-flow/assets/index-dyMV3vMw.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CkpghSPM.js","assets/index-FYOE6RV1.js","assets/index-CPXzLC1X.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-FYOE6RV1.js";const n=r("Preferences",{web:()=>t(()=>import("./web-CkpghSPM.js"),__vite__mapDeps([0,1,2])).then(e=>new e.PreferencesWeb)});export{n as Preferences};
